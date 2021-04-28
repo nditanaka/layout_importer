7147b7f6ee43e146229de24cf1f4664649ad0cb6
nditanaka <tanaching@gmail.com>
2021-04-27 23:23:12 -0400

    plugin init

---
+++ layout_importer.php
@@ -0,0 +1,11 @@
+/**
+ * Plugin Name:       Layout Importer Plugin
+ * Description:       Import and export WordPress Gutenberg block layouts with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Tanaka Chingonzo
+ * Author URI:        https://author.nditanaka.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
